3709bd8b4d85c023d7297c05ae309a658cb54adf
Pescudero <pescudero@C02QC16AFVH6.local>
2018-02-10 15:06:11 +0100

    Move listen to http_socket class

--- server.cc
+++ server.cc
@@ -13,22 +13,25 @@
 
 using namespace std;
 
-void error_control(int error){
-   if(error){
-        cout << " Socket error" << errno;
-        exit(EXIT_FAILURE);
-    } 
-}
 class Http_Socket{
+    
+    void error_control(int error){
+       if(error){
+            cout << " Socket error" << errno;
+            exit(EXIT_FAILURE);
+        } 
+    }
+    
     public:
-    int socket_identifier;
+    int server_socket;
     unsigned short port;
     Http_Socket(unsigned short);
-    int open_http_socket(){
+    
+    int open(){   
+        
         struct protoent *protoent;
         struct sockaddr_in server_address;
         int is_enable = 1;
-        int server_socket;
 
         protoent = getprotobyname("tcp");
         server_socket = socket(AF_INET, SOCK_STREAM, protoent->p_proto);
@@ -49,6 +52,34 @@ class Http_Socket{
 
         return server_socket; 
     }
+    
+    int listen_in_loop(){
+        
+        socklen_t client_lenght;
+        ssize_t bytes_read;
+        int newline = 0;
+        char buffer[BUFSIZ];
+        int client_socket;
+        struct sockaddr_in client_address;
+            
+        while (!errno) {
+            client_lenght = sizeof(client_address);
+            client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_lenght);
+            while ((bytes_read = read(client_socket, buffer, BUFSIZ)) > 0) {
+                cout << "New message: \n";
+                write(STDOUT_FILENO, buffer, bytes_read);
+                if (buffer[bytes_read - 1] == '\n')
+                    //newline;
+                for (int i = 0; i < bytes_read - 1; i++)
+                    buffer[i]++;
+                write(client_socket, buffer, bytes_read);
+                if (newline)
+                    break;
+            }
+            close(client_socket);
+        }
+      return 0;  
+    }
 };
 
 Http_Socket::Http_Socket(unsigned short a){
@@ -56,35 +87,15 @@ Http_Socket::Http_Socket(unsigned short a){
 }
 
 int main() {
-    struct sockaddr_in client_address;
+
     unsigned short port = 1500u;
-    socklen_t client_lenght;
-    ssize_t bytes_read;
-    int newline = 0;
-    char buffer[BUFSIZ];
-    int client_socket, server_socket;
+    int server_socket;
     Http_Socket http_socket(port);
     
-    server_socket = http_socket.open_http_socket(); 
+    http_socket.open(); 
     fprintf(stderr, "Listening on port %d \n", port);
     
-    while (!errno) {
-        client_lenght = sizeof(client_address);
-        client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_lenght);
-        
-        while ((bytes_read = read(client_socket, buffer, BUFSIZ)) > 0) {
-            cout << "New message: \n";
-            write(STDOUT_FILENO, buffer, bytes_read);
-            if (buffer[bytes_read - 1] == '\n')
-                newline;
-            for (int i = 0; i < bytes_read - 1; i++)
-                buffer[i]++;
-            write(client_socket, buffer, bytes_read);
-            if (newline)
-                break;
-        }
-        close(client_socket);
-    }
+    http_socket.listen_in_loop();
     
     return 0;
 }
